01db62a6be52c2e6b905163f984bb58f1511cc12
ƬHE ЯAW ☣ <daraw665@gmail.com>
2023-05-12 19:24:50 +0200

    Create get_upstreams.php

---
+++ get_upstreams.php
@@ -0,0 +1,35 @@
+<?php
+require 'vendor/autoload.php'; // Include the Composer autoloader for Predis
+
+// Connect to Redis
+$redis = new Predis\Client([
+    'host' => '172.69.0.78',
+    'port' => '6379',
+]);
+
+// Get all upstreams keys
+$upstreamsKeys = $redis->keys('nginx_stats:upstreams:*');
+
+// Loop through each upstreams key
+foreach ($upstreamsKeys as $upstreamKey) {
+    echo "Upstream: " . substr($upstreamKey, 20) . "<br>" . PHP_EOL;;
+
+    // Get all peers within the upstream
+    $peers = $redis->hgetall($upstreamKey);
+
+    // Loop through each peer
+    foreach ($peers as $peerId => $peerData) {
+        $peer = json_decode($peerData, true);
+
+        echo "Peer ID: " . $peerId . "<br>" . PHP_EOL;;
+        echo "Server: " . $peer['server'] . "<br>" . PHP_EOL;;
+        echo "State: " . $peer['state'] . "<br>" . PHP_EOL;;
+        echo "Requests: " . $peer['requests'] . "<br>" . PHP_EOL;;
+        // Display additional fields as needed
+
+        echo "<br>" . PHP_EOL;;
+    }
+
+    echo "<br>" . PHP_EOL;;
+}
+?>
